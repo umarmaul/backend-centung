a98d36c6460b570dc5002964ddcfa541040a67c9
umarmaul <122956420+umarmaul@users.noreply.github.com.>
2024-11-15 04:40:21 +0700

    update api seminar

--- backend/routers/log.py
+++ backend/routers/log.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Header
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 import uuid
@@ -13,19 +13,23 @@ router = APIRouter(
     tags=["log"],
 )
 
+
 class LogRequest(BaseModel):
     weight: float = Field(ge=0, le=1000)
-    
+
+
 class ResearchRequest(BaseModel):
     weight: float
     roll: float
     pitch: float
+    weight_prediction: float
+
 
 @router.post("/create-log", status_code=status.HTTP_201_CREATED)
 async def create_log(
     log_data: LogRequest,
     db: Session = Depends(get_db),
-    device: Device = Depends(verify_device)
+    device: Device = Depends(verify_device),
 ):
     new_log = Log(id=uuid.uuid4(), device_id=device.id, weight=log_data.weight)
     db.add(new_log)
@@ -34,11 +38,12 @@ async def create_log(
 
     return {"message": "Log created successfully", "data": new_log}
 
+
 @router.post("/create-research", status_code=status.HTTP_201_CREATED)
 async def create_research(
     research_data: ResearchRequest,
     db: Session = Depends(get_db),
-    device: Device = Depends(verify_device)
+    device: Device = Depends(verify_device),
 ):
     new_research = Research(
         id=uuid.uuid4(),
@@ -46,7 +51,7 @@ async def create_research(
         weight=research_data.weight,
         roll=research_data.roll,
         pitch=research_data.pitch,
-        weight_prediction=0
+        weight_prediction=research_data.weight_prediction,
     )
     db.add(new_research)
     db.commit()
@@ -54,18 +59,18 @@ async def create_research(
 
     return {"message": "Research created successfully", "data": new_research}
 
+
 @router.get("/get-logs", status_code=status.HTTP_200_OK)
 async def get_logs(
-    db: Session = Depends(get_db),
-    device: Device = Depends(verify_device)
+    db: Session = Depends(get_db), device: Device = Depends(verify_device)
 ):
     logs = db.query(Log).filter(Log.device_id == device.id).all()
     return {"data": logs}
 
+
 @router.get("/get-research", status_code=status.HTTP_200_OK)
 async def get_research(
-    db: Session = Depends(get_db),
-    device: Device = Depends(verify_device)
+    db: Session = Depends(get_db), device: Device = Depends(verify_device)
 ):
     researches = db.query(Research).filter(Research.device_id == device.id).all()
     return {"data": researches}
